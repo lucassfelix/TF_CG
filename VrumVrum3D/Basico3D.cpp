28260c2cd5625b81798cf926674a02ae5671baf1
ppwagner <pedro.wagner00@edu.pucrs.br>
2020-06-22 14:34:10 -0300

    Carga de objetos quase pronta e camera pronta

--- VrumVrum3D/Basico3D.cpp
+++ VrumVrum3D/Basico3D.cpp
@@ -11,6 +11,9 @@
 #include <iostream>
 #include <cmath>
 #include <ctime>
+#include <vector>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -146,7 +149,195 @@ void init(void)
 //
 // **********************************************************************
 
-float dirX = 0, dirY = 0, dirZ = 5;
+float obsX = 0, obsY = 1, obsZ = 0;
+float alvoX = obsX+1, alvoY = obsY, alvoZ = obsZ+1;
+float angObsX = 0;
+float angObsY = 0;
+
+void MovimentaObservador(int direcao)
+{
+    switch(direcao)
+    {
+        case 1: //frente
+            glPushMatrix();
+                obsX = obsX+0.7*((cos((angObsX*3.14)/180) + sin((angObsX*3.14)/180)));
+                obsZ = obsZ+0.7*((-(sin((angObsX*3.14)/180)) + cos((angObsX*3.14)/180)));
+                alvoX = alvoX+0.7*((cos((angObsX*3.14)/180) + sin((angObsX*3.14)/180)));
+                alvoZ = alvoZ+0.7*((-(sin((angObsX*3.14)/180)) + cos((angObsX*3.14)/180)));
+            glPopMatrix();
+            break;
+
+        case 2: //trás
+            glPushMatrix();
+                obsX = obsX-0.7*((cos((angObsX*3.14)/180) + sin((angObsX*3.14)/180)));
+                obsZ = obsZ-0.7*((-(sin((angObsX*3.14)/180)) + cos((angObsX*3.14)/180)));
+                alvoX = alvoX-0.7*((cos((angObsX*3.14)/180) + sin((angObsX*3.14)/180)));
+                alvoZ = alvoZ-0.7*((-(sin((angObsX*3.14)/180)) + cos((angObsX*3.14)/180)));
+            glPopMatrix();
+            break;
+
+        case 4: //esquerda
+            angObsX += 6;
+            alvoX = (cos((angObsX*3.14)/180) + sin((angObsX*3.14)/180)) + obsX;
+            alvoZ = (-(sin((angObsX*3.14)/180)) + cos((angObsX*3.14)/180)) + obsZ;
+            break;
+
+        case 3: //direita
+            angObsX -= 6;
+            alvoX = (cos((angObsX*3.14)/180) + sin((angObsX*3.14)/180)) + obsX;
+            alvoZ = (-(sin((angObsX*3.14)/180)) + cos((angObsX*3.14)/180)) + obsZ;
+//            alvoX = ((alvoX - obsX)*cos((-6*3.14)/180) + (alvoZ - obsZ)*sin((-6*3.14)/180)) + obsX;
+//            alvoZ = (-((alvoX - obsX)*sin((-6*3.14)/180)) + (alvoZ - obsZ)*cos((-6*3.14)/180)) + obsZ;
+            break;
+
+        case 5:
+            angObsY -= 6;
+            alvoY = (cos((angObsY*3.14)/180) + sin((angObsY*3.14)/180)) + obsY;
+            alvoZ = (-(sin((angObsY*3.14)/180)) + cos((angObsY*3.14)/180)) + obsZ;
+            break;
+
+        case 6:
+            angObsY += 6;
+            alvoY = (cos((angObsY*3.14)/180) + sin((angObsY*3.14)/180)) + obsY;
+            alvoZ = (-(sin((angObsY*3.14)/180)) + cos((angObsY*3.14)/180)) + obsZ;
+            break;
+    }
+}
+
+vector<int> hexToColor(string Entrada)
+{
+    string aux;
+    int i;
+    vector<int> resp;
+
+    aux = Entrada.substr(2,2);
+    i = (int)strtol(aux.c_str(), NULL,16);
+    resp.push_back(i);
+
+    aux = Entrada.substr(4,2);
+    i = (int)strtol(aux.c_str(), NULL,16);
+    resp.push_back(i);
+
+    aux = Entrada.substr(6,2);
+    i = (int)strtol(aux.c_str(), NULL,16);
+    resp.push_back(i);
+
+    return resp;
+}
+
+typedef struct  // Struct para armazenar um ponto
+{
+    float X,Y,Z;
+    void Set(float x, float y, float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+    void Imprime()
+    {
+        cout << "X: " << X << " Y: " << Y << " Z: " << Z;
+    }
+} TPoint;
+
+
+typedef struct // Struct para armazenar um triângulo
+{
+    TPoint P1, P2, P3;
+    vector<int> cor;
+    void imprime()
+    {
+        cout << "P1 ";  P1.Imprime(); cout << endl;
+        cout << "P2 ";  P2.Imprime(); cout << endl;
+        cout << "P3 ";  P3.Imprime(); cout << endl;
+    }
+} TTriangle;
+
+// Classe para armazenar um objeto 3D
+class Objeto3D
+{
+    vector<TTriangle> faces; // vetor de faces
+    //unsigned int nFaces; // Variavel que armazena o numero de faces do objeto
+public:
+    Objeto3D(string nomearq)
+    {
+        ifstream inFile;
+        inFile.open(nomearq + ".tri");
+        float x;
+        string color;
+        inFile >> x; //joga fora o numero de pontos do txt
+        //esse while sempre vai tirar a primeira cordenada
+        //TODO: fazer com que "X" seja a cor
+        while(inFile >> color)
+        {
+
+            //passar o "color" em um conversor de hex -> rgb (tem no moodle)
+            vector<TPoint> aa;
+            for(int i = 0; i < 3; i++)
+            {
+                TPoint aux;
+                float a,b,c;
+                inFile >> a;
+                inFile >> b;
+                inFile >> c;
+                //cout << a << endl;
+                aux.Set(a,b,c);
+                aa.push_back(aux);
+            }
+
+            TTriangle fig;
+            fig.P1 = aa[0];
+            fig.P2 = aa[1];
+            fig.P3 = aa[2];
+            fig.cor = hexToColor(color);
+            faces.push_back(fig);
+            //cout << "oi" << endl;
+        }
+        inFile.close();
+        //verificar se os pontos estao corretos quando o arquivo .tri estiver pronto para teste
+    }
+
+    unsigned int getNFaces()
+    {
+        return faces.size();
+    }
+
+    void ExibeObjeto()
+    {
+        for(int i = 0; i < faces.size(); i++)
+        {
+            glPushMatrix();
+            glColor3f(1,0,1); //colocar cor do triangulo
+                glBegin(GL_TRIANGLES);
+                    glVertex3f(faces[i].P1.X,faces[i].P1.Y,faces[i].P1.Z);
+                    glVertex3f(faces[i].P2.X,faces[i].P2.Y,faces[i].P2.Z);
+                    glVertex3f(faces[i].P3.X,faces[i].P3.Y,faces[i].P3.Z);
+                glEnd();
+            glPopMatrix();
+        }
+    }
+};
+
+void DesenhaParalelepipedo()
+{
+    glPushMatrix();
+        glColor3ub(130,117,95);
+        glTranslatef(0,-0.95,0);
+        glScalef(1,1,1);
+        DesenhaCubo();
+    glPopMatrix();
+}
+
+void DesenhaPiso()
+{
+    glBegin ( GL_QUADS );
+    glNormal3f(0,1,0);
+    glVertex3f(-1.0f,  0.0f, -1.0f);
+    glVertex3f(-1.0f,  0.0f,  1.0f);
+    glVertex3f( 1.0f,  0.0f,  1.0f);
+    glVertex3f( 1.0f,  0.0f, -1.0f);
+    glEnd();
+}
 
 void PosicUser()
 {
@@ -157,11 +348,110 @@ void PosicUser()
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(dirX, dirY, dirZ,   // Posi‹o do Observador
-              0,0,0,     // Posi‹o do Alvo
+	gluLookAt(obsX, obsY, obsZ,   // Posi‹o do Observador
+              alvoX,alvoY,alvoZ,     // Posi‹o do Alvo
 			  0.0f,1.0f,0.0f);
 
 }
+
+Objeto3D arvore("treeNOVO");
+Objeto3D casa("casaNOVO");
+
+void DesenhaItem(int item)
+{
+    switch(item)
+    {
+    case 1: //rua
+        {
+            DesenhaParalelepipedo();
+            break;
+        }
+
+    case 3: //arvore
+        {
+            glPushMatrix();
+                glScalef(0.4,0.4,0.4);
+                arvore.ExibeObjeto();
+            glPopMatrix();
+            break;
+        }
+
+    case 4: //casa
+        {
+            glPushMatrix();
+                glScalef(1.5,1,1.5); //mudar escala
+                casa.ExibeObjeto();
+            glPopMatrix();
+            break;
+        }
+    //case 5: //sentinel tower
+    }
+}
+
+void DesenhaCenario()
+{
+    ifstream inFile;
+    inFile.open("mapa2.txt");
+    int x,z;
+    inFile >> z;
+    glPushMatrix();
+        glScalef(z,1,z);
+        glColor3ub(59,143,20);
+        DesenhaPiso();
+    glPopMatrix();
+
+    for(int j = 0; j < z; j+=2)
+    {
+        glPushMatrix();
+            glTranslatef(0,0,j);
+            for(int i = 0; i < z; i+=2)
+            {
+                glPushMatrix();
+                    glTranslatef(i,0,0);
+                    inFile >> x;
+                    DesenhaItem(x);
+                glPopMatrix();
+            }
+
+            for(int i = 0; i < z; i+=2)
+            {
+                glPushMatrix();
+                    glTranslatef(i*-1,0,0);
+                    inFile >> x;
+                    DesenhaItem(x);
+                glPopMatrix();
+            }
+        glPopMatrix();
+    }
+
+    for(int j = 0; j < z; j+=2)
+    {
+        glPushMatrix();
+            glTranslatef(0,0,j*-1);
+            for(int i = 0; i < z; i+=2)
+            {
+                glPushMatrix();
+                    glTranslatef(i,0,0);
+                    inFile >> x;
+                    DesenhaItem(x);
+                glPopMatrix();
+            }
+
+            for(int i = 0; i < z; i+=2)
+            {
+                glPushMatrix();
+                    glTranslatef(i*-1,0,0);
+                    inFile >> x;
+                    DesenhaItem(x);
+                glPopMatrix();
+            }
+        glPopMatrix();
+    }
+
+    inFile.close();
+}
+
+
 // **********************************************************************
 //  void reshape( int w, int h )
 //		trata o redimensionamento da janela OpenGL
@@ -191,6 +481,9 @@ void reshape( int w, int h )
 //
 //
 // **********************************************************************
+Objeto3D testezera("house");
+Objeto3D testezera2("ferrariNOVO");
+
 void display( void )
 {
 
@@ -202,19 +495,35 @@ void display( void )
 
 	glMatrixMode(GL_MODELVIEW);
 
-	glPushMatrix();
-		glTranslatef ( 2.0f, 0.0f, 1.0f );
-        glRotatef(AngY,0,1,0);
-		glColor3f(0.5f,0.0f,0.0f); // Vermelho
-		DesenhaCubo();
-	glPopMatrix();
+//	glPushMatrix();
+//		glTranslatef ( 2.0f, 0.0f, 1.0f );
+//        glRotatef(AngY,0,1,0);
+//		glColor3f(0.5f,0.0f,0.0f); // Vermelho
+//		DesenhaCubo();
+//	glPopMatrix();
+//
+//	glPushMatrix();
+//		glTranslatef ( -2.0f, 2.0f, -1.0f );
+//		glRotatef(AngY,0,1,0);
+//		glColor3f(0.0f,0.6f,0.0f); // Verde
+//		DesenhaCubo();
+//	glPopMatrix();
 
 	glPushMatrix();
-		glTranslatef ( -2.0f, 2.0f, -1.0f );
-		glRotatef(AngY,0,1,0);
-		glColor3f(0.0f,0.6f,0.0f); // Verde
-		DesenhaCubo();
+        glScalef(0.4,0.4,0.4);
+        testezera2.ExibeObjeto();
 	glPopMatrix();
+
+    DesenhaCenario();
+
+//    glPushMatrix();
+//        glScalef(0.4,0.4,0.4);
+//        glTranslatef(10,0,10);
+//        arvore.ExibeObjeto();
+//	glPopMatrix();
+
+    cout << "x: " << obsX << " z: " << obsZ << endl;
+    cout << angObsX << " | " << angObsY << endl;
 
 	glutSwapBuffers();
 }
@@ -272,16 +581,42 @@ void keyboard ( unsigned char key, int x, int y )
       exit ( 0 );   // a tecla ESC for pressionada
       break;
     case 'w':
-        dirZ++;
+        MovimentaObservador(1);
         break;
     case 'a':
-        dirX--;
+        MovimentaObservador(4);
         break;
     case 's':
-        dirZ--;
+        MovimentaObservador(2);
         break;
     case 'd':
-        dirX++;
+        MovimentaObservador(3);
+        break;
+
+    case 'q':
+        MovimentaObservador(5);
+        break;
+
+    case 'e':
+        MovimentaObservador(6);
+        break;
+
+    case 'f':
+        if(obsY == 18)
+        {
+            obsX = 0;
+            obsY = 1;
+            obsZ = 0;
+            angObsY = 0;
+        }
+        else
+        {
+            obsX = 3.2;
+            obsY = 18;
+            obsZ = 2.6;
+            angObsX = 180;
+            angObsY  = -90;
+        }
         break;
 
     default:
@@ -335,6 +670,3 @@ int main ( int argc, char** argv )
 	glutMainLoop ( );
 	return 0;
 }
-
-
-
